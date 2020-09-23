7f1fc18c1e7fa7704e7ef6cf3720fbc221f21420
Irene-nandy <irenekasiva75@gmail.com>
2020-09-23 11:08:52 +0300

    add config

--- config.py
+++ config.py
@@ -1,32 +1,32 @@
-# import os
+import os
 
-# class Config:
+class Config:
 
     
-#     SECRET_KEY = '12345'
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = '12345'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-#     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
-#     #  email configurations
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
-
-# class ProdConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-#     pass
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
-# class DevConfig(Config):
-#     DEBUG = True
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
 
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig
-# }
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
 
