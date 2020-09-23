ab98c1ee88558f351735fec950b8135801c9897e
Irene-nandy <irenekasiva75@gmail.com>
2020-09-23 11:09:41 +0300

    modify change

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
+export MAIL_USERNAME='irenekasiva75@gmail.com'
+export MAIL_PASSWORD='irene'
+python3.6 manage.py server
 
-# export MAIL_USERNAME='irenekasiva75@gmail.com'
-# export MAIL_PASSWORD='irene'
-# python3.6 manage.py server
-
-# export SECRET_KEY='12345'
-# python3.6 manage.py server
+export SECRET_KEY='12345'
+python3.6 manage.py server
